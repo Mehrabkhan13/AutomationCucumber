2602d99575fadf4b15c5ea7cf5a186c482b5be39
khanmehrab <ermehrab@gmail.com>
2018-08-07 10:40:57 +0530

    Push all the files

--- src/test/java/com/steps/stepDefinitions/FlightSearchTest.java
+++ src/test/java/com/steps/stepDefinitions/FlightSearchTest.java
@@ -1,10 +1,7 @@
 package com.steps.stepDefinitions;
 
 import java.io.IOException;
-import java.util.List;
-
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
